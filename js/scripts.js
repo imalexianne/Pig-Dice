4d68536607551aa99a1b5a5092e788bb081abb86
Imanirakarama Alexianne <imanirax@yahoo.fr>
2019-01-10 17:39:35 +0200

    add a constructor function for a player

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,10 @@
 //Global Variables
 var player1, player2;
+//Constructor function for a player
+function Player(name, turnTotal, diceRoll, overallScore, active) {
+    this.name = name;
+    this.diceRoll = 0;
+    this.turnTotal = 0;
+    this.overallScore = 0;
+    this.active = active;
+}
