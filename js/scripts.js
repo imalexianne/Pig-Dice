cbe19f681401ff5090a72e99b9bdc8ee1985bd77
Imanirakarama Alexianne <imanirax@yahoo.fr>
2019-01-10 17:41:20 +0200

    add a Function to disable and enable gaming areas according to which player is active.

--- js/scripts.js
+++ js/scripts.js
@@ -8,3 +8,17 @@ function Player(name, turnTotal, diceRoll, overallScore, active) {
     this.overallScore = 0;
     this.active = active;
 }
+//Function to disable and enable gaming areas according to which player is active.
+function activeUser() {
+    if (player1.active === true && player2.active === false) {
+        $('.player1Area').children().prop('disabled', false);
+        $('.player1Area').removeClass('disableGamingArea');
+        $('.player2Area').children().prop('disabled', true);
+        $('.player2Area').addClass('disableGamingArea');
+    } else {
+        $('.player1Area').children().prop('disabled', true);
+        $('.player1Area').addClass('disableGamingArea');
+        $('.player2Area').children().prop('disabled', false);
+        $('.player2Area').removeClass('disableGamingArea');
+    }
+};
